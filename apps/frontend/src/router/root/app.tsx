fb87204db86cb0a608457c8f931e3db428311ff6
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-09 03:29:17 +0200

    Title - we (I mean me) are starting!

--- apps/frontend/src/router/root/app.tsx
+++ apps/frontend/src/router/root/app.tsx
@@ -1,11 +1,19 @@
 import { Button, Checkbox } from "#ui";
+import { cls } from "#utils";
 
 export default function App() {
 	return (
 		<main className="grid">
 			<div className="row">
-				<div className="col-12 d-flex justify-content-center txt-size-7 txt-weight-1">
-					<h1>Hello Multiverse!</h1>
+				<div
+					className={cls(
+						"col-12",
+						"d-flex flex-column align-items-end",
+						"txt-size-8 txt-lh-small txt-weight-1",
+					)}
+				>
+					<div>Uszata</div>
+					<div>Kuchnia</div>
 				</div>
 			</div>
 		</main>
