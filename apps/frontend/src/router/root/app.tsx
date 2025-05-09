c69543f2c5b85007374d9abe6827212191aaf4e1
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-09 03:12:42 +0200

    Move app to router

---
+++ apps/frontend/src/router/root/app.tsx
@@ -0,0 +1,13 @@
+import { Button, Checkbox } from "#ui";
+
+export default function App() {
+	return (
+		<main className="grid">
+			<div className="row">
+				<div className="col-12 d-flex justify-content-center txt-size-7 txt-weight-1">
+					<h1>Hello Multiverse!</h1>
+				</div>
+			</div>
+		</main>
+	);
+}
