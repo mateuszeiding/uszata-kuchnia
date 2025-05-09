8c95c4fbf013c1af673ac338eb43559e8f596635
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-10 01:47:42 +0200

    Add swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,9 +6,21 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// SWAGGER
+	const config = new DocumentBuilder()
+		.setTitle("Cats example")
+		.setDescription("The cats API description")
+		.setVersion("1.0")
+		.addTag("cats")
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, documentFactory);
+
 	const globalPrefix = "api";
 	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.PORT || 3000;
