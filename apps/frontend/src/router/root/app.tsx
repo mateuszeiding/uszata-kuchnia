709a56159c4e4b314429e783f6b161b7a0a7ec06
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-09 20:29:08 +0200

    Base layout

--- apps/frontend/src/router/root/app.tsx
+++ apps/frontend/src/router/root/app.tsx
@@ -1,21 +1,30 @@
-import { Button, Checkbox } from "#ui";
 import { cls } from "#utils";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
 	return (
 		<main className="grid">
-			<div className="row">
+			<div className="col-8">
+				<div className="container">
+					<Outlet />
+				</div>
+			</div>
+			<aside
+				className={cls(
+					"col-4",
+					"txt-size-8 txt-lh-small txt-weight-1",
+					"border-left",
+				)}
+			>
 				<div
-					className={cls(
-						"col-12",
-						"d-flex flex-column align-items-end",
-						"txt-size-8 txt-lh-small txt-weight-1",
-					)}
+					className={cls("py-7", "d-flex flex-column align-items-end flex-1")}
 				>
 					<div>Uszata</div>
 					<div>Kuchnia</div>
 				</div>
-			</div>
+				<hr />
+				<nav>MENU</nav>
+			</aside>
 		</main>
 	);
 }
