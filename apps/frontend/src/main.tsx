c06775de86dffd46359ed0115fb79faa47aa55ad
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-09 03:13:05 +0200

    Import all font weights

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router";
 import GlobalProvider from "./context/GlobalProvider";
-import "@fontsource/geist-mono";
+import "@fontsource/geist-mono/latin.css";
 import "#styles";
 
 const container = document.getElementById("root");
