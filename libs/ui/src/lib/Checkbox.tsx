28c2ace7709c83d315d9fbb4b4545770dd4c37a3
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-09 03:11:19 +0200

    Switch

--- libs/ui/src/lib/Checkbox.tsx
+++ libs/ui/src/lib/Checkbox.tsx
@@ -1,19 +1,25 @@
 import { cls } from "#utils";
-import { useId, type InputHTMLAttributes } from "react";
+import { useId, useRef, type InputHTMLAttributes } from "react";
 
 type Checkbox = {
 	label: string;
+	crossed?: boolean;
+	switch?: boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export default function ({ ...args }: Checkbox) {
+export default function ({ ...args }: Readonly<Checkbox>) {
 	const id = useId();
+	const ref = useRef<HTMLInputElement>(undefined);
 	return (
 		<div className="d-flex flex-row align-items-center g-col-3">
 			<input
 				id={id}
+				ref={ref}
 				{...{ ...args }}
 				type="checkbox"
-				className={cls("checkbox", args.className)}
+				role={args.switch ? "switch" : "checkbox"}
+				aria-checked={ref.current?.checked}
+				className={cls("checkbox", args.crossed && "crossed", args.className)}
 			/>
 			<label htmlFor={id}>{args.label}</label>
 		</div>
