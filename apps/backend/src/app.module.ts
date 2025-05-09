14b1f7eb238fea064652451d8743dd3c3c4586a4
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-10 01:48:45 +0200

    Add cqrs

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { CqrsModule } from "@nestjs/cqrs";
 import { AppController } from "./controllers/app.controller";
 import { AppService } from "./services/app.service";
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [ConfigModule.forRoot(), CqrsModule.forRoot()],
 	controllers: [AppController],
 	providers: [AppService],
 })
