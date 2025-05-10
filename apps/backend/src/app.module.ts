45548823fcc91dc897402b48706686221bbf6a70
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-05-10 02:11:03 +0200

    Create prisma service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CqrsModule } from "@nestjs/cqrs";
 import { AppController } from "./controllers/app.controller";
-import { AppService } from "./services/app.service";
+import { AppService, PrismaService } from "./services/!export";
 
 @Module({
 	imports: [ConfigModule.forRoot(), CqrsModule.forRoot()],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, PrismaService],
 })
 export class AppModule {}
